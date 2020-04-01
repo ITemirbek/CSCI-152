29fed4dfe202bee40ccc92ecbb2655e8814fa3aa
Kassymzhomart Kunanbayev <kasymmkk@gmail.com>
2020-04-02 05:47:49 +0600

    check one by one task

--- Assignment4/set.cpp
+++ Assignment4/set.cpp
@@ -98,90 +98,90 @@ size_t log_base2( size_t s ){
    return value;
 }
 
-const treenode* find( const treenode* n, int i ){
-   treenode* curr = n;
-   while(curr!=nullptr){
-      if(curr->val == i){
-         return curr;
-         break;
-      }
-      else if(curr->val > i)
-         curr = curr->left;
-      else if(curr->val < i)
-         curr = curr->right;      
-   }
-   return curr;
-}
+// const treenode* find( const treenode* n, int i ){
+//    treenode* curr = n;
+//    while(curr!=nullptr){
+//       if(curr->val == i){
+//          return curr;
+//          break;
+//       }
+//       else if(curr->val > i)
+//          curr = curr->left;
+//       else if(curr->val < i)
+//          curr = curr->right;      
+//    }
+//    return curr;
+// }
 
-treenode** find( treenode** n, int i ){
-   treenode* curr = *n;
-   while(curr->left != nullptr & curr->right != nullptr){
-      if(curr->val == i){
-         return *curr;
-         break;
-      }
-      else if(curr->val > i)
-         curr = curr->left;
-      else if(curr->val < i)
-         curr = curr->right;      
-   }
-   return *curr;
-}
+// treenode** find( treenode** n, int i ){
+//    treenode* curr = *n;
+//    while(curr->left != nullptr & curr->right != nullptr){
+//       if(curr->val == i){
+//          return *curr;
+//          break;
+//       }
+//       else if(curr->val > i)
+//          curr = curr->left;
+//       else if(curr->val < i)
+//          curr = curr->right;      
+//    }
+//    return *curr;
+// }
 
-// PROOFREADING IS NEEDED
-bool set::insert(int i){
-   treenode* curr = find( tr, i );
-   bool status = TRUE
-   if(curr->val == i)
-      status = FALSE;
-   else if(curr->val>i){
-      curr = curr->left;
-      curr->val = i;
-   }
-   else{
-      curr = curr->right;
-      curr->val = i;      
-   }
-   return status;
-}
+// // PROOFREADING IS NEEDED
+// bool set::insert(int i){
+//    treenode* curr = find( tr, i );
+//    bool status = TRUE
+//    if(curr->val == i)
+//       status = FALSE;
+//    else if(curr->val>i){
+//       curr = curr->left;
+//       curr->val = i;
+//    }
+//    else{
+//       curr = curr->right;
+//       curr->val = i;      
+//    }
+//    return status;
+// }
 
-bool set::contains( int i ) const{
-   if(find(tr, i)==nullptr)
-      return FALSE; 
-   return TRUE;
-}
+// bool set::contains( int i ) const{
+//    if(find(tr, i)==nullptr)
+//       return FALSE; 
+//    return TRUE;
+// }
 
 
-bool set::remove( int i ){
-   treenode* curr = find(tr, i);
-   if (curr->val ==i){
-      if(curr->left != nullptr & curr->right != nullptr){
-         treenode temp = removerightmost(curr);
-         curr->val = temp->val;
-         curr = temp;
-         delete temp;
-      }
-      if(curr->left == nullptr & curr->right == nullptr){
-         delete curr;
-         curr = nullptr;
-      }else if(curr->left != nullptr){
-         treenode* temp = curr;
-         curr = curr ->left;
-         delete temp;
-      }else if(curr->right != nullptr){
-         treenode* temp = curr;
-         curr = curr ->right;
-         delete temp;
-      }
-      return TRUE;
-   }
-   return FALSE;   
-}
+// bool set::remove( int i ){
+//    treenode* curr = find(tr, i);
+//    if (curr->val ==i){
+//       if(curr->left != nullptr & curr->right != nullptr){
+//          treenode temp = removerightmost(curr);
+//          curr->val = temp->val;
+//          curr = temp;
+//          delete temp;
+//       }
+//       if(curr->left == nullptr & curr->right == nullptr){
+//          delete curr;
+//          curr = nullptr;
+//       }else if(curr->left != nullptr){
+//          treenode* temp = curr;
+//          curr = curr ->left;
+//          delete temp;
+//       }else if(curr->right != nullptr){
+//          treenode* temp = curr;
+//          curr = curr ->right;
+//          delete temp;
+//       }
+//       return TRUE;
+//    }
+//    return FALSE;   
+// }
 
-treenode* removerightmost( treenode** from ){
-   treenode temp = from->right;
-   while(temp->left!=nullptr){
-      temp = temp->left
-   }
-   return temp;
-}
+// treenode* removerightmost( treenode** from ){
+//    treenode temp = from->right;
+//    while(temp->left!=nullptr){
+//       temp = temp->left
+//    }
+//    return temp;
+// }
